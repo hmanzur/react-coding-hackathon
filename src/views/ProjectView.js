fa701b139328d625238c74e252f4c6c60397e149
Habid Enrique Manzur Restrepo <habibmanzur@outlook.com>
2023-08-28 16:38:42 -0500

    feat(ci): deployment updates

--- src/views/ProjectView.js
+++ src/views/ProjectView.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   List,
-  ListItem, 
-  ListItemText, 
-  ListItemAvatar, 
-  Avatar, 
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
   ListItemSecondaryAction,
   IconButton,
   Icon
@@ -14,61 +14,61 @@ import {
 import ProjectService from '../services/ProjectService'
 
 export default function ProjectItemSide(props) {
-    const [project, setProject] = useState({repositories:[]});
+  const [project, setProject] = useState({ repositories: [] });
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const {projectId} = props.match.params;
+  const { projectId } = props.match.params;
 
-    const [reload] = useState(false);
-    
+  const [reload] = useState(false);
+
+  useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true)
-      const result  = await ProjectService.getById(projectId)
+      const result = await ProjectService.getById(projectId)
 
       setProject(result)
       setIsLoading(false)
     };
 
-    useEffect(() => {
-      fetchData()
-    }, [reload])
+    fetchData()
+  }, [reload])
 
-    if (isLoading) {
-      return (<div>Loading...</div>)
-    }
-    
-    return (
-      <div>
-          <h1>
-            <Avatar alt={project.language} src={`/static/languages/${project.language}.svg`} /> {project.name}
-          </h1>
+  if (isLoading) {
+    return (<div>Loading...</div>)
+  }
 
-          <p>{project.description}</p>
+  return (
+    <div>
+      <h1>
+        <Avatar alt={project.language} src={`/static/languages/${project.language}.svg`} /> {project.name}
+      </h1>
 
-          <p>Status {project.is_enabled ? 'Enabled': 'Disabled'}</p>
+      <p>{project.description}</p>
 
-          <h2>Repositories</h2>
-          <List>
-            {project.repositories.map(item => (
-                <ListItem key={item.id}>
-                    <ListItemAvatar>
-                        <Avatar>
-                            <Icon>{item.repo_type}</Icon>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={item.name} secondary={`Type ${item.repo_type}`} />
+      <p>Status {project.is_enabled ? 'Enabled' : 'Disabled'}</p>
 
-                    <ListItemSecondaryAction>
-                        <a target="_blank" href={item.link_url} >
-                            <IconButton edge="end" aria-label="link">
-                                <Icon>open_in_new</Icon>
-                            </IconButton>
-                        </a>
-                    </ListItemSecondaryAction>
-                </ListItem>
-                ))}
-          </List>
-      </div>
-    );
+      <h2>Repositories</h2>
+      <List>
+        {project.repositories.map(item => (
+          <ListItem key={item.id}>
+            <ListItemAvatar>
+              <Avatar>
+                <Icon>{item.repo_type}</Icon>
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={item.name} secondary={`Type ${item.repo_type}`} />
+
+            <ListItemSecondaryAction>
+              <a target="_blank" href={item.link_url} rel="noopener noreferrer">
+                <IconButton edge="end" aria-label="link">
+                  <Icon>open_in_new</Icon>
+                </IconButton>
+              </a>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 }
